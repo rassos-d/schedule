4755b44b951da1f6328e2466f0a79612fc89973a
rassos-d <multidomennyjvzvod@gmail.com>
2025-07-24 11:19:43 +0400

    Add semster and studyYears

--- backend/Scheduler.Dto/Constants/Semester.cs
+++ backend/Scheduler.Dto/Constants/Semester.cs
@@ -7,4 +7,10 @@ public enum Semester
     Third = 3,
     Fourth = 4,
     Fiveth = 5,
+    Sixth = 6,
+    Seventh = 7,
+    Eighth = 8,
+    Ninth = 9,
+    Tenth = 10,
+    Eleventh = 11
 }
--- backend/Scheduler.Dto/Constants/StudyYear.cs
+++ backend/Scheduler.Dto/Constants/StudyYear.cs
@@ -5,4 +5,7 @@ public enum StudyYear
     First = 1,
     Second = 2,
     Third = 3,
+    Fourth = 4,
+    Fifth = 5,
+    Sixth = 6
 }
